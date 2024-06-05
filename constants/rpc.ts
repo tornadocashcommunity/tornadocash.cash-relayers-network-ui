5fa9ce517b575a08ae8ff896e4b215395c82270a
Theo <theo@noreply.org>
2024-06-05 06:27:05 -0700

    Change RPC to self-hosted

--- constants/rpc.ts
+++ constants/rpc.ts
@@ -1,5 +1,5 @@
 import { ChainId } from '@/types'
 
 export const RPC_LIST: { [chainId in ChainId]: string } = {
-  [ChainId.MAINNET]: 'https://mainnet.chainnodes.org/d692ae63-0a7e-43e0-9da9-fe4f4cc6c607',
+  [ChainId.MAINNET]: 'https://tornadocash-rpc.com',
 }
